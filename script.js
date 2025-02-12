46a0aaa9d1763b6336a504b805384b2299b84a77
JD3Ent <jjd32004@gmail.com>
2025-02-12 06:20:40 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 document.getElementById("dark-mode-toggle").addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
 });
+
+/* Dark Mode Styling */
+const darkMode = document.createElement("style");
+darkMode.innerHTML = `
+.dark-mode {
+    background: #222;
+    color: white;
+}
+
+.dark-mode section {
+    background: #333;
+}
+
+.dark-mode h2 {
+    color: lightblue;
+}
+
+.dark-mode nav ul li a {
+    color: cyan;
+}
+
+.dark-mode .btn {
+    background: cyan;
+    color: black;
+}
+`;
+document.head.appendChild(darkMode);
